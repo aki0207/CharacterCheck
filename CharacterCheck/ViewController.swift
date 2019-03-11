90755e1b9e18947298fea83d66429e4c7caa4132
aki0207 <forever.look.aki@docomo.ne.jp>
2019-03-11 13:57:59 +0900

    正規表現変えれば他のも対応できそう

--- CharacterCheck/ViewController.swift
+++ CharacterCheck/ViewController.swift
@@ -1,18 +1,43 @@
-//
-//  ViewController.swift
-//  CharacterCheck
-//
-//  Created by ききょ on 2019/03/11.
-//  Copyright © 2019 kikyo. All rights reserved.
-//
-
 import UIKit
+
+extension String {
+    func isAlphanumeric() -> Bool {
+        return NSPredicate(format: "SELF MATCHES %@","[0-9]+" ).evaluate(with:self)
+    }
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var alfabet = "abc"
+        var japanse = "123えお"
+        var number = "123"
+        
+        
+        if alfabet.isAlphanumeric() {
+            print("数字やん")
+        } else {
+            print("数字じゃないやん")
+        }
+        
+        if japanse.isAlphanumeric() {
+            print("数字やん")
+        } else {
+            print("数字じゃないやん")
+        }
+        
+        
+        if number.isAlphanumeric() {
+            print("数字やん")
+        } else {
+            print("数字じゃないやん")
+        }
+        
+        
+        
+        
     }
 
 
